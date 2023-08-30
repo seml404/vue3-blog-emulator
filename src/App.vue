64e6d74481855016827f18d24ccf82c4e33eb89b
seml <4040591@gmail.com>
2023-08-30 15:08:20 +0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    test
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
