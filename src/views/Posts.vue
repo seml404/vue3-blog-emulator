16056055419624a4d8646a8268a16149af82dede
seml <4040591@gmail.com>
2023-09-05 18:07:52 +0300

    added load more posts

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -6,28 +6,38 @@
         :custom_placeholder="'Search'"
         :model-value="searchValue"
       ></input-main>
-      <btn-main
-        class="is-primary mt-4 mb-4"
-        @click="searchPost"
-        v-if="!searchedValue"
-        :disabled="!searchValue"
-        >Search</btn-main
-      >
-      <btn-main class="is-primary mt-4 mb-4" @click="() => (searchValue = '')" v-else
-        >Clear search</btn-main
-      >
+
+      <div class="is-flex is-justify-content-center is-align-content-center p-4">
+        <btn-main
+          class="is-primary"
+          @click="searchPost"
+          v-if="!searchedValue"
+          :disabled="!searchValue"
+          >Search for post</btn-main
+        >
+        <btn-main class="is-primary mt-4 mb-4" @click="() => (searchValue = '')" v-else
+          >Clear search</btn-main
+        >
+        <btn-main @click="showModal = true">Create new post</btn-main>
+      </div>
     </div>
     <div class="is-flex is-justify-content-center is-align-content-center">
       <p class="is-size-2">List of posts</p>
     </div>
-    <btn-main @click="showModal = true">Create new post</btn-main>
 
     <Transition name="posts-fade">
       <div v-if="posts.length">
         <posts-list :posts="posts" @deletePost="handleDeletePost"></posts-list>
         <div ref="bottom" class="card has-text-centered">End of list</div>
       </div>
-      <div v-else>No posts yet</div>
+      <div v-else>
+        <div class="is-flex is-justify-content-center is-align-content-center">
+          <p class="is-size-3">No posts yet</p>
+        </div>
+        <btn-main class="is-primary mt-4 mb-4" @click="() => requestForPosts()"
+          >Request for posts</btn-main
+        >
+      </div>
     </Transition>
     <Transition name="modal-fade">
       <modal-window :showItem="showModal" @closeModal="showModal = false"
